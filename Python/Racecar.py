66d19925a38c6007b469467d6718bb1a622ca870
AnnaEdmondson <puckpranks.cluefinder@gmail.com>
2018-10-25 15:09:02 -0600

    python

--- Python/Racecar.py
+++ Python/Racecar.py
@@ -1,71 +1,86 @@
 import maya.cmds as cmds
 
 obj = cmds.polyCube (w=1, h=1, d=1, sx=3, sy=1, sz=1, ax=[0,1,0], cuv=4, ch=1, name="Body_Geo")[0]
-scale -r 7.073064 7.073064 7.073064 ;
-scale -r 1 1 1.841102 ;
-polyCylinder -r 1 -h 2 -sx 20 -sy 1 -sz 1 -ax 0 1 0 -rcp 0 -cuv 3 -ch 1 -name "Front_Axel_Geo";
-select -addFirst Front_Axel_Geo ;
-setAttr "polyCylinder1.subdivisionsAxis" 8;
-scale -r 1 6.306826 1 ;
-setAttr "Front_Axel_Geo.rotateZ" 90;
-move -r 0 -1.951478 4.548751 ;
-duplicate -rr;
-move -r 0 0 -9.614203 ;
-polyCylinder -r 1 -h 2 -sx 20 -sy 1 -sz 1 -ax 0 1 0 -rcp 0 -cuv 3 -ch 1 -name "Wheel_Geo";
-setAttr "polyCylinder2.subdivisionsAxis" 8;
-setAttr "Wheel_Geo.rotateZ" 90;
-move -r 6.902812 -1.805158 6.098474 ;
-move -r 0 0 -1.541351 ;
-scale -r 4.37802 1 4.37802 ;
-select -r Wheel_Geo.f[8:15] ;
-polyExtrudeFacet -constructionHistory 1 -keepFacesTogether 1 -pvx 7.902812 -pvy -1.805157739 -pvz 4.55712313 -divisions 1 -twist 0 -taper 1 -off 0 -thickness 0 -smoothingAngle 30 Wheel_Geo.f[8:15];
-scale -r -p 7.902812cm -1.805158cm 4.557123cm 0.415771 1 0.415771 ;
-select -r Wheel_Geo.f[8:15] ;
-polyExtrudeFacet -constructionHistory 1 -keepFacesTogether 1 -pvx 7.902812 -pvy -1.805158 -pvz 4.557123 -divisions 1 -twist 0 -taper 1 -off 0 -thickness 0 -smoothingAngle 30 Wheel_Geo.f[8:15];
-setAttr "polyExtrudeFace2.localTranslate" -type double3 0 0 -1.147426 ;
-select -r Wheel_Geo ;
-duplicate -rr;
-move -r 0 0 -9.484245 ;
-select -r Wheel_Geo ;
-select -tgl Wheel_Geo1 ;
-duplicate -rr;
-move -r -13.879012 0 0 ;
-select -r Wheel_Geo2 ;
-select -tgl Wheel_Geo3 ;
-rotate -r -os -fo 0 0 180 ;
-polySphere -r 1 -sx 20 -sy 20 -ax 0 1 0 -cuv 2 -ch 1 -name "Windshield_Geo";
-setAttr "polySphere1.subdivisionsAxis" 8;
-setAttr "polySphere1.subdivisionsHeight" 8;
-move -r 0 3.450431 0 ;
-scale -r 1 1 6.44611 ;
-scale -r 3.466357 1 1 ;
-scale -r 1 2.531111 1 ;
-move -r 0 -0.331939 0 ;
-polyCone -r 1 -h 2 -sx 20 -sy 1 -sz 0 -ax 0 1 0 -rcp 0 -cuv 3 -ch 1 -name "Front_Spike_Geo";
-setAttr "polyCone1.subdivisionsAxis" 8;
-setAttr "Front_Spike_Geo.rotateX" 90;
-move -r 0 0 7.408706 ;
-scale -r 3.439762 3.439762 3.439762 ;
-move -r 0 0 2.153823 ;
-select -r Body_Geo.f[6:8] ;
-polyExtrudeFacet -constructionHistory 1 -keepFacesTogether 1 -pvx 0 -pvy 0 -pvz -6.511116138 -divisions 1 -twist 0 -taper 1 -off 0 -thickness 0 -smoothingAngle 30 Body_Geo.f[6:8];
-setAttr "polyExtrudeFace3.localTranslate" -type double3 0 0 5.317829 ;
-rotate -r -eu -p 0cm 0cm -11.828945cm -fo 35.674137 0 0 ;
-polyExtrudeFacet -constructionHistory 1 -keepFacesTogether 1 -pvx 0 -pvy 0 -pvz -6.511116138 -divisions 1 -twist 0 -taper 1 -off 0 -thickness 0 -smoothingAngle 30 Body_Geo.f[2];
-select -r Windshield_Geo ;
-select -tgl Front_Spike_Geo ;
-select -tgl Body_Geo ;
-move -r 0 0 2.747234 ;
-polyCone -r 1 -h 2 -sx 20 -sy 1 -sz 0 -ax 0 1 0 -rcp 0 -cuv 3 -ch 1 -name "Wheel_Spike_Geo";
-setAttr "polyCone2.subdivisionsAxis" 8;
-setAttr "Wheel_Spike_Geo.rotateZ" 270;
-move -r 8.717874 1.156298 4.428271 ;
-duplicate -rr;
-move -r 0 -6.012648 0 ;
-duplicate -rr;
-move -r 0 2.821105 3.204342 ;
-duplicate -rr;
-move -r 0 0 -5.816862 ;
+cmds.scale (7.073064, 7.073064, 7.073064, obj, r=True)
+cmds.scale (1, 1, 1.841102, obj, r=True)
+
+obj1 = cmds.polyCylinder (r=1, h=2, sx=8, sy=1, sz=1, ax=[0, 1, 0], rcp=0, cuv=3, ch=1, name="Front_Axel_Geo")
+cmds.select ("Front_Axel_Geo")
+cmds.scale (1, 6.306826, 1, obj1, r=True)
+cmds.setAttr ("Front_Axel_Geo.rotateZ", 90)
+cmds.move (0, -1.951478, 4.548751, obj1, r=True)
+cmds.duplicate (rr=True)
+cmds.move (0, 0, -9.614203, r=True)
+
+obj2 = cmds.polyCylinder (r=1, h=2, sx=8, sy=1, sz=1, ax=[0, 1, 0], rcp=0, cuv=3, ch=1, name="Wheel_Geo")
+cmds.setAttr ("Wheel_Geo.rotateZ", 90)
+cmds.move (6.902812, -1.805158, 6.098474, obj2, r=True)
+cmds.move (0, 0, -1.541351, obj2, r=True)
+cmds.scale (4.37802, 1, 4.37802, obj2, r=True)
+cmds.select ("Wheel_Geo.f[8:15]", r=True)
+cmds.polyExtrudeFacet ("Wheel_Geo.f[8:15]", constructionHistory=1, keepFacesTogether=1, pvx=7.902812, pvy=-1.805157739, pvz=4.55712313, divisions=1, twist=0, taper=1, off=0, thickness=0, smoothingAngle=30)
+cmds.scale (0.415771, 1, 0.415771)
+cmds.select ("Wheel_Geo.f[8:15]" r=True)
+cmds.polyExtrudeFacet ("Wheel_Geo.f[8:15]", constructionHistory=1, keepFacesTogether=1, pvx=7.902812, pvy=-1.805158, pvz=4.557123, divisions=1, twist=0, taper=1, off=0, thickness=0, smoothingAngle=30)
+
+cmds.setAttr ("polyExtrudeFace2.localTranslate" type=double[3, 0, 0, -1.147426]
+
+cmds.select ("Wheel_Geo", r=True)
+cmds.duplicate (rr=True)
+cmds.move (0, 0, -9.484245, r=True)
+cmds.select ("Wheel_Geo", r=True)
+
+cmds.select (tgl=Wheel_Geo1)
+
+cmds.duplicate (rr=True)
+cmds.move (-13.879012, 0, 0, r=True)
+cmds.select ("Wheel_Geo2", r=True)
+
+cmds.select (tgl=Wheel_Geo3)
+
+cmds.rotate (r -os fo=[0, 0, 180])
+
+obj3 = cmds.polySphere (r=1, sx=20, sy=20, ax=[0, 1, 0], cuv=2, ch=1, name="Windshield_Geo")
+cmds.setAttr ("polySphere1.subdivisionsAxis", 8)
+cmds.setAttr ("polySphere1.subdivisionsHeight", 8)
+cmds.move (0, 3.450431, 0, r=True)
+cmds.scale (1, 1, 6.44611, r=True)
+cmds.scale (3.466357, 1, 1, r=True)
+cmds.scale (1, 2.531111, 1, r=True)
+cmds.move (0, -0.331939, 0, r=True)
+
+obj4 = cmds.polyCone (r=1, h=2, sx=20, sy=1, sz=0, ax=[0, 1, 0], rcp=0, cuv=3, ch=1, name="Front_Spike_Geo")
+cmds.setAttr ("polyCone1.subdivisionsAxis", 8)
+cmds.setAttr ("Front_Spike_Geo.rotateX", 90)
+
+cmds.move (0, 0, 7.408706, r=True)
+cmds.scale (3.439762, 3.439762, 3.439762, obj4, r=True)
+cmds.move (0, 0, 2.153823, r=True)
+cmds.select ("Body_Geo.f[6:8]", r=True)
+cmds.polyExtrudeFacet ("Body_Geo.f[6:8]", constructionHistory=1, keepFacesTogether=1, pvx=0, pvy=0, pvz=-6.511116138, divisions=1, twist=0, taper=1, off=0, thickness=0, smoothingAngle=30)
+
+cmds.setAttr "polyExtrudeFace3.localTranslate" -type double3 0 0 5.317829 ;
+
+cmds.rotate -r -eu -p 0cm 0cm -11.828945cm -fo 35.674137 0 0 ;
+
+cmds.polyExtrudeFacet ("Body_Geo.f[2]", constructionHistory=1, keepFacesTogether=1, pvx=0, pvy=0, pvz=-6.511116138, divisions=1, twist=0, taper=1, off=0, thickness=0, smoothingAngle=30)
+cmds.select ("Windshield_Geo", r=True)
+
+cmds.select -tgl Front_Spike_Geo ;
+cmds.select -tgl Body_Geo ;
+
+cmds.move (0, 0, 2.747234, r=True)
+
+obj5 = cmds.polyCone (r=1, h=2, sx=20, sy=1, sz=0, ax=[0, 1, 0], rcp=0, cuv=3, ch=1, name="Wheel_Spike_Geo")
+cmds.setAttr ("polyCone2.subdivisionsAxis", 8)
+cmds.setAttr ("Wheel_Spike_Geo.rotateZ", 270)
+cmds.move (8.717874, 1.156298, 4.428271, r=True)
+cmds.duplicate (rr=True)
+cmds.move (0, -6.012648, 0, r=True)
+cmds.duplicate (rr=True)
+cmds.move (0, 2.821105, 3.204342, r=True)
+cmds.duplicate (rr=True)
+cmds.move (0, 0, -5.816862, r=True)
 select -r Wheel_Spike_Geo3 Wheel_Spike_Geo2 Wheel_Spike_Geo1 Wheel_Spike_Geo ;
 duplicate -rr;
 move -r 0 0 -9.609049 ;
